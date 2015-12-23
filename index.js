a6215e79648ca9090aab2731f2ed952ddc9bb0e8
M.C. van den Boogaart <scola@kvdb.name>
2015-12-23 10:34:20 +0100

    Implement token authorization

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ const Views = require('@scola/views');
 
 const Base = require('./lib/base');
 const Password = require('./lib/password');
+const Token = require('./lib/token');
 
 class Client extends DI.Module {
   configure() {
     this.addModule(Views.Module);
     this.addModule(Password.Client.Module);
+    this.addModule(Token.Client.Module);
   }
 }
 
 class Server extends DI.Module {
   configure() {
     this.addModule(Password.Server.Module);
+    this.addModule(Token.Server.Module);
   }
 }
 
 module.exports = {
   Base,
   Password,
+  Token,
   Module: {
     Client,
     Server
