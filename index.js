c705d28f81cd11bcc53daf62ca020fdb190e3bb7
M.C. van den Boogaart <scola@kvdb.name>
2016-01-06 22:03:11 +0100

    Redesign based on new mva module

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 'use strict';
 
-const DI = require('@scola/di');
-const Views = require('@scola/views');
-
-const Base = require('./lib/base');
-const Password = require('./lib/password');
-const Token = require('./lib/token');
-
-class Client extends DI.Module {
-  configure() {
-    this.addModule(Views.Module);
-    this.addModule(Password.Client.Module);
-    this.addModule(Token.Client.Module);
-  }
-}
-
-class Server extends DI.Module {
-  configure() {
-    this.addModule(Password.Server.Module);
-    this.addModule(Token.Server.Module);
-  }
-}
-
 module.exports = {
-  Base,
-  Password,
-  Token,
-  Module: {
-    Client,
-    Server
-  }
+  Client: require('./lib/client'),
+  Server: require('./lib/server')
 };
